35448cda0c3f7abbb8394403e5b5e737cbff2b0e
Samuel Palacios Bernate <sdpalaciob@eafit.edu.co>
2020-10-23 17:51:55 -0500

    Add matrix forms

--- calculator/forms.py
+++ calculator/forms.py
@@ -1,6 +1,19 @@
 from django import forms
 
-class MatrixDimensionForm(forms.Form):
-    row_number = forms.IntegerField(max_value = 10)
-    column_number = forms.IntegerField(max_value = 10)
 
+class MatrixDimensionForm(forms.Form):
+    row_number = forms.IntegerField(min_value=2, max_value=10)
+    column_number = forms.IntegerField(min_value=2, max_value=10)
+
+
+OPERATION_CHOICES = (
+    ("determinant", "Determinant"),
+    ("rank", "Rank"),
+    ("scalar", "Scalar"),
+    ("transpose", "Transpose"),
+)
+
+
+class OperationForm(forms.Form):
+    operation = forms.ChoiceField(required=True, choices=OPERATION_CHOICES)
+    scalar = forms.IntegerField(required=False)
